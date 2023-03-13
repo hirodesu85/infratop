bbf2c56ecb2628da2c9bbe7e859a72bc2de18c39
EC2 Default User <ec2-user@ip-172-31-23-58.ec2.internal>
2023-03-13 10:27:31 +0000

    認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+    
+  devise_for :customers, skip: [:password], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  root to: 'public/homes#top'
+  
+  
+  scope module: :public do
+    get '/mypage' => 'customers#show'
+  end
+  
+  
+  devise_for :admins, path: 'admin', skip: [:registrations, :password], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
