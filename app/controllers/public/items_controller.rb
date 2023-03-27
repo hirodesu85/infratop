1f85be148e800f9a2e7e9b61d4fee7cbfe6443a3
EC2 Default User <ec2-user@ip-172-31-23-58.ec2.internal>
2023-03-27 02:47:16 +0000

    ジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.where(is_sold: true)
+    @genres = Genre.all
+    if params[:genre_id]
+      @items = Item.where(is_sold: true).where(genre_id: params[:genre_id])
+    else
+      @items = Item.where(is_sold: true)
+    end
   end
 
   def show
