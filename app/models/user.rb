6d8bb5088e81e565787cc71a2a89e2e41b366872
OGATA Hiromu <hiromukkun@gmail.com>
2024-02-11 16:58:34 +0900

    add:bootstrap, fontawesome導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
 end
