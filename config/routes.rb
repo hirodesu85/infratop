213572950442710a9334b0324250d7e926a1e14b
OGATA Hiromu <hiromukkun@gmail.com>
2024-02-17 01:01:33 +0900

    add:コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, except: [:new] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
