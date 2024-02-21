a8ded9cb966d205a9aec0ec7244b59e40b5b7df2
OGATA Hiromu <hiromukkun@gmail.com>
2024-02-21 12:35:00 +0900

    add:管理者のログイン、ログアウト実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,13 @@ Rails.application.routes.draw do
     get 'about' => 'homes#about'
     get 'customers/my_page' => 'customers#show', as: :my_page
   end
-
+  
   # Admin側のルーティング
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
