ebd40bb6d499b55b86d46246369d2f6f1e15bc09
OGATA Hiromu <hiromukkun@gmail.com>
2024-02-22 13:32:29 +0900

    add:退会機能実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,15 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  private
+
+  def customer_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    return if customer.nil?
+    return unless customer.valid_password?(params[:customer][:password])
+    if !customer.is_active
+      flash[:alert] = 'お客様は退会済みです。'
+      redirect_to new_customer_session_path
+    end
+  end
 end
