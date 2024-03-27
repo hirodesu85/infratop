8567bac2782417ae825975a66c91d5f49ff9949d
OGATA Hiromu <hiromukkun@gmail.com>
2024-03-27 19:08:34 +0900

    add:サンプルデータをseedファイルに記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# Admin.create!(
-#     email: "a@a",
-#     password: "aaaaaa"
-# )
-
-Genre.create!(
-    name: "クッキー"
+Customer.create!(
+	email: "example@gmail.com",
+	password: "foobar",
+	last_name: "山田",
+	first_name: "太郎",
+	last_name_kana: "ヤマダ",
+	first_name_kana: "タロウ",
+	postal_code: "1234567",
+	address: "東京都渋谷区",
+	telephone_number: "09012345678"
 )
 
-item = Item.create!(
-    genre_id: 13,
-    name: "たまごプリン",
-    introduction: "プリンです",
-    price: 500,
-    is_active: true
+Admin.create!(
+  email: "a@a",
+  password: "aaaaaa"
 )
 
+genres = %w[プリン ケーキ クッキー]
+
+genres.each do |name|
+  Genre.create!(name: name)
+end
+
+item = Genre.first.items.build(
+	name: "チーズケーキ",
+	introduction: "濃厚な味わい",
+	price: 500
+)
 item.image.attach(io: File.open(Rails.root.join('app/assets/images/pudding.jpg')), filename: 'pudding.jpeg')
+
+item.save!
+
